700f4a7b646bfc98bdc8c864add4e0f63944468a
ursasimenc <ursasimenc17@gmail.com>
2023-07-29 16:26:15 +0200

    minor fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -69,7 +69,7 @@
 					>
 				</div>
 				<div class="reason-text">
-					<h4>Best quality</h4>
+					<h3>Best quality</h3>
 					<p>Discover an endless variety of the world’s best artisan coffee from each of our roasters.</p>
 				</div>
 			</div>
@@ -83,7 +83,7 @@
 					>
 				</div>
 				<div class="reason-text">
-					<h4>Exclusive benefits</h4>
+					<h3>Exclusive benefits</h3>
 					<p>Special offers and swag when you subscribe, including 30% off your first shipment.</p>
 				</div>
 			</div>
@@ -97,7 +97,7 @@
 					>
 				</div>
 				<div class="reason-text">
-					<h4>Free shipping</h4>
+					<h3>Free shipping</h3>
 					<p>We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.</p>
 				</div>
 			</div>
@@ -424,7 +424,7 @@
 					.reason-text {
 						@include flex(center, start, column, 24px);
 
-						h4 {
+						h3 {
 							@include h4();
 						}
 
